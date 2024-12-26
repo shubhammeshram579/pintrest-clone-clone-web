d800f4bd02cb51aae0d01667f57c5713240ed728
Shubham Meshram <105120972+shubhammeshram579@users.noreply.github.com>
2024-12-27 02:09:31 +0530

    updated acce control

--- backend/App.js
+++ backend/App.js
@@ -26,6 +26,17 @@ app.use(cors({
   }));
 
 
+  app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://pintrest-clone-frontend.vercel.app"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+
+
 
 // app.use(cors());
 // some midelware use file confifration
